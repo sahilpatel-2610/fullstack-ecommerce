c3e65e8785f07534a2a2e0b4a113a3d2b647d048
sahilpatel-2610 <kakadiyasahil102@gmail.com>
2025-12-30 14:05:37 +0530

    Updated admin and app files

--- server/app.js
+++ server/app.js
@@ -41,7 +41,9 @@ require('dotenv').config();
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Routes
 const categoriesRoutes = require('./routes/categories');
