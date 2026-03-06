cb83317a7f76b4bd76bdbe7c7bbd027d61be2da9
sahilpatel-2610 <kakadiyasahil102@gmail.com>
2026-03-06 20:34:17 +0530

    Updated

--- client/src/Components/HomeCat/index.js
+++ client/src/Components/HomeCat/index.js
@@ -1,4 +1,90 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import { Navigation } from 'swiper/modules';
+
+// const HomeCat = (props) => {
+
+//     // const [itemBg, setItemBg] = useState([
+//     //     '#fffceb',
+//     //     '#ecffec',
+//     //     '#feefea',
+//     //     '#fff3eb',
+//     //     '#fff3ff',
+//     //     '#f2fce4',
+//     //     '#feefea',
+//     //     '#fffceb',
+//     //     '#feefea',
+//     //     '#ecffec',
+//     //     '#feefea',
+//     //     '#fff3eb',
+//     //     '#fff3ff',
+//     //     '#f2fce4',
+//     //     '#feefea',
+//     //     '#fffceb',
+//     //     '#feefea',
+//     //     '#ecffec'
+//     // ]);
+
+//     return(
+//         <section className="homeCat">
+//             <div className="container">
+//                 <h3 className="mb-3 hd">Featured Categories</h3>
+//                 <Swiper
+//                     slidesPerView={10}
+//                     spaceBetween={8}
+//                     navigation={true}
+//                     slidesPerGroup={3}
+//                     pagination={{
+//                     clickable: true,
+//                     }}
+//                     modules={[Navigation]}
+//                     className="mySwiper"
+//                 >
+//                 {/* {
+//                     itemBg?.map((item,index) => {
+//                         return(
+//                             <SwiperSlide>
+//                                 <div className="item text-center cursor" style={{background: item}}>
+//                                   <img src={catData?.images[0]}/>
+
+//                                   <h6>{catData?.name}</h6>
+//                                 </div>
+//                             </SwiperSlide>
+
+//                         )
+//                     })
+//                 } */}
+
+//                 {
+//                         props.catData?.categoryList?.length!==0 && props.catData?.categoryList?.map((cat,index) => {
+//                             return(
+//                                 <SwiperSlide key={index}>
+//                                     <div className="item text-center cursor" style={{background: cat.color}}>
+//                                         <img src={cat.images[0]}/>
+
+//                                         <h6>{cat.name}</h6>
+//                                     </div>
+//                                 </SwiperSlide>
+
+//                             )
+//                         })
+//                 }
+
+                
+                   
+                    
+//                 </Swiper>  
+//             </div>
+//         </section>
+//     )
+// }
+
+// export default HomeCat;
+
+
+import React from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -6,75 +92,39 @@ import { Navigation } from 'swiper/modules';
 
 const HomeCat = (props) => {
 
-    // const [itemBg, setItemBg] = useState([
-    //     '#fffceb',
-    //     '#ecffec',
-    //     '#feefea',
-    //     '#fff3eb',
-    //     '#fff3ff',
-    //     '#f2fce4',
-    //     '#feefea',
-    //     '#fffceb',
-    //     '#feefea',
-    //     '#ecffec',
-    //     '#feefea',
-    //     '#fff3eb',
-    //     '#fff3ff',
-    //     '#f2fce4',
-    //     '#feefea',
-    //     '#fffceb',
-    //     '#feefea',
-    //     '#ecffec'
-    // ]);
-
     return(
         <section className="homeCat">
             <div className="container">
                 <h3 className="mb-3 hd">Featured Categories</h3>
+
                 <Swiper
                     slidesPerView={10}
                     spaceBetween={8}
                     navigation={true}
                     slidesPerGroup={3}
-                    pagination={{
-                    clickable: true,
-                    }}
                     modules={[Navigation]}
                     className="mySwiper"
                 >
-                {/* {
-                    itemBg?.map((item,index) => {
-                        return(
-                            <SwiperSlide>
-                                <div className="item text-center cursor" style={{background: item}}>
-                                  <img src={catData?.images[0]}/>
-
-                                  <h6>{catData?.name}</h6>
-                                </div>
-                            </SwiperSlide>
-
-                        )
-                    })
-                } */}
 
                 {
-                        props.catData?.categoryList?.length!==0 && props.catData?.categoryList?.map((cat,index) => {
-                            return(
-                                <SwiperSlide key={index}>
-                                    <div className="item text-center cursor" style={{background: cat.color}}>
-                                        <img src={cat.images[0]}/>
+                    props.catData?.categoryList?.length > 0 &&
+                    props.catData.categoryList.map((cat,index) => (
+                        <SwiperSlide key={index}>
+                            <div 
+                                className="item text-center cursor" 
+                                style={{ background: cat.color }}
+                            >
+                                <img 
+                                    src={cat.images?.[0]} 
+                                    alt={cat.name} 
+                                />
 
-                                        <h6>{cat.name}</h6>
-                                    </div>
-                                </SwiperSlide>
-
-                            )
-                        })
+                                <h6>{cat.name}</h6>
+                            </div>
+                        </SwiperSlide>
+                    ))
                 }
 
-                
-                   
-                    
                 </Swiper>  
             </div>
         </section>
